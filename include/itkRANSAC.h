07d57c56fa910c0dd10d4d21e7064f9c1a6e2dbc
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-12-21 17:10:08 -0500

    Check for RMSE when inlier count is same

--- include/itkRANSAC.h
+++ include/itkRANSAC.h
@@ -203,6 +203,7 @@ private:
   // agrees with the best model, otherwise false
   bool *       bestVotes;
   unsigned int numVotesForBest;
+  double       bestRMSE;
 
   std::vector<T> data;
   std::vector<T> agreeData;
